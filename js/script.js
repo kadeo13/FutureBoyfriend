5f8da06eca76cb2fe75691672aea26edb0d4dfa5
Katie O'Leary <kadeo13@gmail.com>
2018-08-29 16:16:37 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -44,4 +44,11 @@ $(document).ready(function() {
   $(document).bind("mobileinit", function() {
     $.mobile.ajaxEnabled = false;
   })
+
+  var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+
+  if (isSafari) {
+    $(".other").addClass("hidden");
+    $(".safari").removeClass("hidden");
+  }
 })
